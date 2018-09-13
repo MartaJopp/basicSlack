ecff1b87ae77bd09b2bcaf7cd34273e4b55b4d63
Marta Jopp <martapeterson@gmail.com>
2018-09-12 22:04:38 -0500

    channel component

---
+++ components/channels/Channel.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class Channel extends Component {
+    onClick(e) {
+        e.preventDefault();
+        const {setChannel, channel} = this.props;
+        setChannel(channel);
+    }
+    render() {
+        const {channel} = this.props;
+        return (
+            <li>
+                <a onClick={this.onClick.bind(this)}>
+                    {channel.name}
+                </a>
+            </li>
+        )
+    }
+}
+
+Channel.propTypes = {
+    //required property
+    channel: React.propTypes.object.isRequired,
+    setChannel: React.propTypes.func.isRequired
+}
+
+export default Channel;
