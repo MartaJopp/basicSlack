048afcb4c7655bceb8acdfcbab520b3b095add16
Marta Jopp <martapeterson@gmail.com>
2018-09-12 22:18:58 -0500

    ChannelList component

---
+++ components/channels/ChannelList.jsx
@@ -0,0 +1,25 @@
+import React, {component} from 'react';
+import Channel from './Channel.jsx';
+
+class ChannelList extends Component {
+    render(){
+        return (
+            <ul>{
+                this.props.channels.map( chan =>{
+                    <Channel
+                        channel={chan}
+                        setChannel={this.props.setChannel}
+                    />
+                })
+            }
+            </ul>
+        )
+    }
+}
+
+ChannelList.propTypes = {
+    channels: React.PropTypes.array.isRequired,
+    setChannel: React.PropTypes.func.isRequired
+}
+
+export default ChannelList
